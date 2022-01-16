14f02044c4f5dbf135b725e02716d9b47ef61c9e
ROllerozxa <temporaryemail4meh+github@gmail.com>
2022-01-16 21:04:49 +0100

    Remove some useless items.

--- mods/dye/init.lua
+++ mods/dye/init.lua
@@ -2,9 +2,6 @@
 
 dye = {}
 
--- Load support for MT game translation.
-local S = minetest.get_translator("dye")
-
 -- Make dye names and descriptions available globally
 
 dye.dyes = {
@@ -24,40 +21,3 @@ dye.dyes = {
 	{"magenta",    "Magenta"},
 	{"pink",       "Pink"},
 }
-
--- Define items
-
-for _, row in ipairs(dye.dyes) do
-	local name = row[1]
-	local description = row[2]
-	local groups = {dye = 1}
-	groups["color_" .. name] = 1
-
-	minetest.register_craftitem("dye:" .. name, {
-		inventory_image = "dye_" .. name .. ".png",
-		description = S(description .. " Dye"),
-		groups = groups
-	})
-end
-
--- Dummy calls to S() to allow translation scripts to detect the strings.
--- To update this run:
--- for _,e in ipairs(dye.dyes) do print(("S(%q)"):format(e[2].." Dye")) end
-
---[[
-S("White Dye")
-S("Grey Dye")
-S("Dark Grey Dye")
-S("Black Dye")
-S("Violet Dye")
-S("Blue Dye")
-S("Cyan Dye")
-S("Dark Green Dye")
-S("Green Dye")
-S("Yellow Dye")
-S("Brown Dye")
-S("Orange Dye")
-S("Red Dye")
-S("Magenta Dye")
-S("Pink Dye")
---]]
