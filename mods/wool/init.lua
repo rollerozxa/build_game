f81c983b01647b1e4834e13b5ee4ad9eec74d5a6
ROllerozxa <temporaryemail4meh+github@gmail.com>
2022-01-17 20:38:27 +0100

    Move dyes into wool mod

--- mods/wool/init.lua
+++ mods/wool/init.lua
@@ -3,7 +3,23 @@
 -- Load support for MT game translation.
 local S = minetest.get_translator("wool")
 
-local dyes = dye.dyes
+local dyes = {
+	{"white",      "White"},
+	{"grey",       "Grey"},
+	{"dark_grey",  "Dark Grey"},
+	{"black",      "Black"},
+	{"violet",     "Violet"},
+	{"blue",       "Blue"},
+	{"cyan",       "Cyan"},
+	{"dark_green", "Dark Green"},
+	{"green",      "Green"},
+	{"yellow",     "Yellow"},
+	{"brown",      "Brown"},
+	{"orange",     "Orange"},
+	{"red",        "Red"},
+	{"magenta",    "Magenta"},
+	{"pink",       "Pink"},
+}
 
 for i = 1, #dyes do
 	local name, desc = unpack(dyes[i])
@@ -16,37 +32,4 @@ for i = 1, #dyes do
 				flammable = 3, wool = 1},
 		sounds = default.node_sound_defaults(),
 	})
-
-	minetest.register_craft{
-		type = "shapeless",
-		output = "wool:" .. name,
-		recipe = {"group:dye,color_" .. name, "group:wool"},
-	}
 end
-
--- Legacy
--- Backwards compatibility with jordach's 16-color wool mod
-minetest.register_alias("wool:dark_blue", "wool:blue")
-minetest.register_alias("wool:gold", "wool:yellow")
-
--- Dummy calls to S() to allow translation scripts to detect the strings.
--- To update this run:
--- for _,e in ipairs(dye.dyes) do print(("S(%q)"):format(e[2].." Wool")) end
-
---[[
-S("White Wool")
-S("Grey Wool")
-S("Dark Grey Wool")
-S("Black Wool")
-S("Violet Wool")
-S("Blue Wool")
-S("Cyan Wool")
-S("Dark Green Wool")
-S("Green Wool")
-S("Yellow Wool")
-S("Brown Wool")
-S("Orange Wool")
-S("Red Wool")
-S("Magenta Wool")
-S("Pink Wool")
---]]
