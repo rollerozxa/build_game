c9374aab05273fedeeb80379fc0e95e81b45eeb5
ROllerozxa <temporaryemail4meh+github@gmail.com>
2022-01-27 21:41:49 +0100

    Add wired fence node.

---
+++ mods/bldg_nodes/init.lua
@@ -0,0 +1,17 @@
+
+minetest.register_node("bldg_nodes:wired_fence", {
+	description = "Wired fence",
+	tiles = {"bldg_nodes_wired_fence.png"},
+	use_texture_alpha = "clip",
+	drawtype = "nodebox",
+	paramtype = "light",
+	paramtype2 = "facedir",
+	node_box = {
+		type = "fixed",
+		fixed = {
+			{-0.5, -0.5, 0.5, 0.5, 0.5, 0.5}, -- NodeBox1
+		}
+	},
+	groups = {snappy=3},
+	sounds = default.node_sound_stone_defaults(),
+})
