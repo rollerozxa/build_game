9d535f2d72efebd1859af51c4d602ba311fa6d79
ROllerozxa <temporaryemail4meh+github@gmail.com>
2022-01-27 19:43:46 +0100

    Add WIP build game API for easing custom node creation.

---
+++ mods/bldg_api_nodes/init.lua
@@ -0,0 +1,20 @@
+
+buildgame = {}
+
+function buildgame.register_node(def)
+	-- Shorthand, which is disabled if someone uses the long form
+	local name
+	if not def.name:find(":") then
+		name = minetest.get_current_modname()..":"..def.name
+	else
+		name = def.name
+	end
+
+	def.groups = def.groups or { snappy = 3 }
+
+	if type(def.tiles) == "string" then
+		def.tiles = { def.tiles }
+	end
+
+	minetest.register_node(name, def)
+end
