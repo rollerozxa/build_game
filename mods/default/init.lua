1fdffa0ddb1e4496cd5b4ea796e68b9769526f7d
ROllerozxa <temporaryemail4meh+github@gmail.com>
2022-02-23 20:24:21 +0100

    Move include() into separate bldg_api mod.

--- mods/default/init.lua
+++ mods/default/init.lua
@@ -32,18 +32,6 @@ minetest.override_item("", {
 	}
 })
 
--- Less verbose file include function taken from NodeCore
--- https://gitlab.com/sztest/nodecore/-/blob/master/mods/nc_api/init.lua
-local include = rawget(_G, "include") or function(...)
-	local parts = {...}
-	table.insert(parts, 1, minetest.get_modpath(minetest.get_current_modname()))
-	if parts[#parts]:sub(-4) ~= ".lua" then
-		parts[#parts] = parts[#parts] .. ".lua"
-	end
-	return dofile(table.concat(parts, "/"))
-end
-rawset(_G, "include", include)
-
 -- Load files
 include("functions")
 include("trees")
