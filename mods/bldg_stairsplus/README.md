771f16faee402e510633e224b69753412f0aeb80
ROllerozxa <temporaryemail4meh+github@gmail.com>
2022-04-03 21:36:35 +0200

    Readd stairsplus as bldg_stairsplus

---
+++ mods/bldg_stairsplus/README.md
@@ -0,0 +1,2 @@
+# `bldg_stairsplus`
+Part of Build Game. Based on Stairs+ as part of moreblocks, separated into its own mod.
--- mods/bldg_stairsplus/registrations.lua
+++ mods/bldg_stairsplus/registrations.lua
@@ -8,22 +8,30 @@ Licensed under the zlib license. See LICENSE.md for more information.
 -- default registrations
 local default_nodes = { -- Default stairs/slabs/panels/microblocks:
 	"stone",
-	"stone_block",
 	"cobble",
+	"stone_brick",
+	"stone_block",
 	"mossycobble",
-	"brick",
-	"sandstone",
-	"steelblock",
-	"goldblock",
-	"copperblock",
-	"bronzeblock",
-	"diamondblock",
-	"tinblock",
 	"desert_stone",
-	"desert_stone_block",
 	"desert_cobble",
-	"meselamp",
-	"glass",
+	"desert_stone_brick",
+	"desert_stone_block",
+	"sandstone",
+	"sandstone_brick",
+	"sandstone_block",
+	"desert_sandstone",
+	"desert_sandstone_brick",
+	"desert_sandstone_block",
+	"silver_sandstone",
+	"silver_sandstone_brick",
+	"silver_sandstone_block",
+	"obsidian",
+	"obsidian_brick",
+	"obsidian_block",
+
+	"dirt",
+	"ice",
+
 	"tree",
 	"wood",
 	"pine_tree",
@@ -32,26 +40,23 @@ local default_nodes = { -- Default stairs/slabs/panels/microblocks:
 	"acacia_wood",
 	"aspen_tree",
 	"aspen_wood",
-	"obsidian",
-	"obsidian_block",
-	"obsidianbrick",
-	"stonebrick",
-	"desert_stonebrick",
-	"sandstonebrick",
-	"silver_sandstone",
-	"silver_sandstone_brick",
-	"silver_sandstone_block",
-	"desert_sandstone",
-	"desert_sandstone_brick",
-	"desert_sandstone_block",
-	"sandstone_block",
+
+	"steel_block",
+	"copper_block",
+	"tin_block",
+	"bronze_block",
+	"gold_block",
+	"diamond_block",
+
 	"coral_skeleton",
-	"ice",
-	"dirt",
+
+	"glass",
+	"brick",
+	"meselamp",
 }
 
 for _, name in pairs(default_nodes) do
-	local mod = "default"
+	local mod = "bldg_nodes"
 	local nodename = mod .. ":" .. name
 	local ndef = table.copy(minetest.registered_nodes[nodename])
 	ndef.sunlight_propagates = true
@@ -97,7 +102,7 @@ stairsplus:register_alias_all("moreblocks", "split_stone_tile_alt", "moreblocks"
 -- conflicts with another node and so the alias for that specific node gets
 -- ignored.
 minetest.register_lbm({
-	name = "moreblocks:fix_split_stone_tile_alt_name_collision",
+	name = "bldg_stairsplus:fix_split_stone_tile_alt_name_collision",
 	nodenames = {"moreblocks:stair_split_stone_tile_alt"},
 	action = function(pos, node)
 		minetest.set_node(pos, {
