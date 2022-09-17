7f355b5ec551fd1ef0b48e429565e380d72dec93
ROllerozxa <temporaryemail4meh+github@gmail.com>
2022-09-17 20:14:25 +0200

    Readd chests and furnaces as purely decorative nodes

--- mods/bldg_nodes/default.lua
+++ mods/bldg_nodes/default.lua
@@ -1051,6 +1051,46 @@ local nodes = {
 		is_ground_content = false,
 		sounds = sound_glass,
 		light_source = minetest.LIGHT_MAX,
+	},
+
+	-- Decorative container nodes
+	chest = {
+		description = "Chest",
+		tiles = {
+			"default_chest_top.png", "default_chest_top.png",
+			"default_chest_side.png", "default_chest_side.png",
+			"default_chest_front.png", "default_chest_side.png"},
+		paramtype2 = "facedir",
+		no_stairs = true,
+	},
+	furnace = {
+		description = "Furnace",
+		tiles = {
+			"default_furnace_top.png", "default_furnace_bottom.png",
+			"default_furnace_side.png", "default_furnace_side.png",
+			"default_furnace_side.png", "default_furnace_front.png"},
+		paramtype2 = "facedir",
+		no_stairs = true,
+	},
+	furnace_active = {
+		description = "Furnace (Active)",
+		tiles = {
+			"default_furnace_top.png", "default_furnace_bottom.png",
+			"default_furnace_side.png", "default_furnace_side.png",
+			"default_furnace_side.png",
+			{
+				image = "default_furnace_front_active.png",
+				backface_culling = false,
+				animation = {
+					type = "vertical_frames",
+					aspect_w = 16,
+					aspect_h = 16,
+					length = 1.5
+				},
+			}
+		},
+		paramtype2 = "facedir",
+		no_stairs = true,
 	}
 }
 
