5b33738c2d26a8bdc002cc363158b4b4f7abec5d
ROllerozxa <temporaryemail4meh+github@gmail.com>
2022-09-18 21:07:20 +0200

    Fix hand wield image, merge pickblock, no support for ass-old formspec versions lolz

--- mods/bldg_core/init.lua
+++ mods/bldg_core/init.lua
@@ -4,27 +4,28 @@
 bldg = {}
 
 minetest.override_item("", {
+	wield_image = "wieldhand.png",
 	wield_scale = { x = 1, y = 1, z = 2.5 },
 	tool_capabilities = {
 		max_drop_level = 0,
 		groupcaps = {}
-	}
+	},
+	on_place = function(itemstack, placer, pointed_thing)
+		if minetest.is_creative_enabled(placer:get_player_name()) then
+			local pointed_node = minetest.get_node(pointed_thing.under)
+			return pointed_node
+		end
+	end
 })
 
 -- GUI related stuff
 minetest.register_on_joinplayer(function(player)
 	-- Set formspec prepend
-	local formspec = [[
-			bgcolor[#080808BB;true]
-			listcolors[#00000069;#5A5A5A;#141318;#30434C;#FFF] ]]
-	local name = player:get_player_name()
-	local info = minetest.get_player_information(name)
-	if info.formspec_version > 1 then
-		formspec = formspec .. "background9[5,5;1,1;i3_bg_full.png;true;10]"
-	else
-		formspec = formspec .. "background[5,5;1,1;i3_bg_full.png;true]"
-	end
-	player:set_formspec_prepend(formspec)
+	player:set_formspec_prepend([[
+		bgcolor[#080808BB;true]
+		listcolors[#00000069;#5A5A5A;#141318;#30434C;#FFF]
+		background9[5,5;1,1;i3_bg_full.png;true;10]
+	]])
 end)
 
 dofile(minetest.get_modpath('bldg_core').."/creative.lua")
